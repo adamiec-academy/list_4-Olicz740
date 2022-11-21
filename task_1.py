9dc884939e58d8267510e9cf7a8468b2568bc1df
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-21 21:29:45 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,10 @@
 def is_palindrome(text):
-    return False
+    text = text.lower()
+    x = text.split()
+    for i in range(0, int(text[len(text)/2])):
+        if text[i] == text[len(text)+1]:
+            text[len(text)] = text[len(text)] - 1
+            return True
+        else:
+            return False
+print(is_palindrome("ala"))
