5b2b8734f749a937019d8645fdb3ed7f280d5ba7
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-24 20:37:30 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,11 +1,11 @@
 def min_max(data):
-    min = 0
+    min = 1
     max = 0
     for i in range(len(data)):
         if data[i] > max:
             max = data[i]
     for i in range(len(data)):
-        if data[i] < max:
+        if data[i] <= max and data[i] < min:
             min = data[i]
 
-    return min , max
+    return min, max
