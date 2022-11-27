2407b95d9e41a3c99956182a149c4b87b2c75209
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-27 16:41:42 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,10 +1,9 @@
 def is_palindrome(text):
     text = text.lower()
-    text = list(text)
-    lista =[]
-    for i in text:
-        lista.insert(0, i)
-    for i in range(len(text)):
-        if lista[i] == text[i]:
+    text = text.split()
+    text = "".join(text)
+    lista = text[::-1]
+    for i in range(len(text) // 2):
+        if lista == text:
             return True
     return False
